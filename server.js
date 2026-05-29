1e01cb997d831ee7e186d2b6928663f186880e61
SaugatXthaa <shresthakhiralal@gmail.com>
2026-05-29 10:29:32 +0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const addonInterface = require("./addon");
-
-// Render/Railway provide the port through an environment variable
-const port = process.env.PORT || 7000;
-
-serveHTTP(addonInterface, { port: port });
-console.log(`Addon is active on port ${port}`);
