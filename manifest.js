7b0a31fe34debad91e1f6056b0179078de115754
SaugatXthaa <shresthakhiralal@gmail.com>
2026-05-29 10:11:02 +0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,10 @@
-
+const manifest = {
+    id: "community.youraddon.name",
+    version: "1.0.0",
+    name: "My Addon",
+    description: "Multi-source provider",
+    resources: ["stream"], // Resources must be an array
+    types: ["movie", "series"], // Types must be an array
+    catalogs: [], // <--- THIS MUST BE AN EMPTY ARRAY LIKE THIS
+    idPrefixes: ["tt"]
+};
