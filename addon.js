30dfbee54d5af43fdc8492c04ecf3ce0097a00c9
SaugatXthaa <shresthakhiralal@gmail.com>
2026-05-29 10:01:21 +0400

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,1 +1,19 @@
+const { addonBuilder } = require("stremio-addon-sdk");
 
+const manifest = {
+    id: "community.legal.template",
+    version: "1.0.0",
+    name: "Stremio Template",
+    resources: ["stream"],
+    types: ["movie"],
+    idPrefixes: ["tt"]
+};
+
+const builder = new addonBuilder(manifest);
+
+builder.defineStreamHandler((args) => {
+    // Return empty or sample streams for testing
+    return Promise.resolve({ streams: [] });
+});
+
+module.exports = builder.getInterface();
