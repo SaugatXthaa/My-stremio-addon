13eafcd72e69dc4bea84754b1e7f0eaaee40a94d
SaugatXthaa <shresthakhiralal@gmail.com>
2026-05-29 10:15:02 +0400

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,19 +1,41 @@
 const { addonBuilder } = require("stremio-addon-sdk");
 
 const manifest = {
-    id: "community.legal.template",
+    id: "community.multisource.addon",
     version: "1.0.0",
-    name: "Stremio Template",
+    name: "Multi-Source 4K Addon",
+    description: "Streams from Multi-Sources with 4K support",
     resources: ["stream"],
-    types: ["movie"],
-    idPrefixes: ["tt"]
+    types: ["movie", "series"],
+    idPrefixes: ["tt"],
+    catalogs: [] // FIXED: This was causing your error
 };
 
 const builder = new addonBuilder(manifest);
 
 builder.defineStreamHandler((args) => {
-    // Return empty or sample streams for testing
-    return Promise.resolve({ streams: [] });
+    // This is where you would normally run your scraper logic.
+    // The 'url' below should be replaced with the link you extract from the sites.
+    const streams = [
+        {
+            name: "Multi-Source",
+            title: "4K Ultra HD\nMulti-Subtitles",
+            url: "https://example.com/direct_video_link.m3u8", // Direct video link goes here
+            behaviorHints: {
+                notWebReady: false,
+            },
+            // How to add integrated subtitles
+            subtitles: [
+                {
+                    id: "eng",
+                    url: "https://example.com/english_subs.vtt",
+                    lang: "English"
+                }
+            ]
+        }
+    ];
+
+    return Promise.resolve({ streams: streams });
 });
 
 module.exports = builder.getInterface();
